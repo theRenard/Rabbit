eb3290376c25ccbdea08103485e7c7abdf28b0a6
Daniele Tabanella <daniele.tabanella@gmail.com>
2019-10-19 23:13:44 +0200

    feat: add colors

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 #include <DRV8833.h>
 #include <NeoPixelBus.h>
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 #include "avdweb_Switch.h"
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
+#include "Ticker.h"
+
 
 /** PINS ASSIGNEMENTS **/
 const uint8_t
@@ -24,10 +32,27 @@ const uint8_t
   COLOR_SATURATION = 128,
   DFPLAYER_VOLUME = 20; // 0 => 30
 
+// MILLIS
+unsigned long initial_millis;
+unsigned long current_millis;
+const unsigned long MQTT_DELAY = 5000;
+
+// WIFI INSTANCE
+WiFiManager wifiManager;
+
+// MQTT INSTANCE
+WiFiClient espClient;
+PubSubClient MQTTclient(espClient);
+#define MQTT_SERVER "homebridge"
+
+// ARDUINO JSON INSTANCE
+const size_t capacity = JSON_ARRAY_SIZE(5) + 3*JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(4) + 70;
+DynamicJsonDocument MQTTSubPayload(capacity);
+
 // SWITCH INSTANCES
-Switch headButton = Switch(PIN_HEAD_BUTTON, INPUT_PULLUP, LOW); // not working (inverted GND VCC on board)
-Switch leftMotorOpto = Switch(PIN_MOTOR_LEFT_OPTO, INPUT, HIGH); // now working (broken lcd)
-Switch rightMotorOpto = Switch(PIN_MOTOR_RIGHT_OPTO, INPUT, HIGH);
+// Switch headButton = Switch(PIN_HEAD_BUTTON, INPUT_PULLUP, LOW); // not working (inverted GND VCC on board)
+// Switch leftMotorOpto = Switch(PIN_MOTOR_LEFT_OPTO, INPUT, HIGH); // now working (broken lcd)
+// Switch rightMotorOpto = Switch(PIN_MOTOR_RIGHT_OPTO, INPUT, HIGH);
 
 // NEOPIXEL INSTANCE
 NeoPixelBus<NeoGrbFeature, NeoEsp8266Dma800KbpsMethod> strip(PIXEL_COUNT);
@@ -48,9 +73,73 @@ SoftwareSerial mySoftwareSerial(PIN_DFPLAYER_RX, PIN_DFPLAYER_TX); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 void printDetail(uint8_t type, int value);
 
-// ** SETUP
-// ** SETUP
-// ** SETUP
+//* FUNCTIONS *//
+//* FUNCTIONS *//
+//* FUNCTIONS *//
+
+boolean reconnect() {
+  if (MQTTclient.connect("Rabbit")) {
+    Serial.println("MQTT is connected...");
+    MQTTclient.subscribe("iot/rabbit");
+  }
+  return MQTTclient.connected();
+}
+
+void MQTTcallback(char* topic, byte* payload, unsigned int length) {
+
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  deserializeJson(MQTTSubPayload, payload, length);
+
+  driver.motorAForward();
+  driver.motorBForward();
+
+  if (MQTTSubPayload.containsKey("sound")) {
+
+    JsonArray array = MQTTSubPayload["sound"].as<JsonArray>();
+    for(JsonVariant v : array) {
+
+      for (uint16_t pixel = 0; pixel < PIXEL_COUNT; pixel++) {
+          RgbColor color = RgbColor(random(255), random(255), random(255));
+          strip.SetPixelColor(pixel, color);
+      }
+      strip.Show();
+
+      Serial.println(v.as<int>());
+      myDFPlayer.play(v.as<int>());
+      int stt = myDFPlayer.readState();
+      int type = myDFPlayer.readType();
+      while (stt == 529 || stt == 513 || stt == 512) {
+        stt = myDFPlayer.readState();
+        type = myDFPlayer.readType();
+        Serial.println( stt );
+        Serial.println( type );
+        delay(300);
+        if (type == 11) break;
+      }
+    }
+  }
+
+  driver.motorAStop();
+  driver.motorBStop();
+
+  for (uint16_t pixel = 0; pixel < PIXEL_COUNT; pixel++) {
+      RgbColor color = RgbColor(0, 0, 0);
+      strip.SetPixelColor(pixel, color);
+  }
+  strip.Show();
+
+}
+
+//* SETUP *//
+//* SETUP *//
+//* SETUP *//
 
 void setup() {
   mySoftwareSerial.begin(9600);
@@ -73,205 +162,54 @@ void setup() {
   driver.attachMotorA(PIN_MOTOR_RIGHT_1, PIN_MOTOR_RIGHT_2);  // RIGHT_MOTOR
   driver.attachMotorB(PIN_MOTOR_LEFT_1, PIN_MOTOR_LEFT_2);    // LEFT_MOTOR
 
+  //reset saved settings
+  //wifiManager.resetSettings();
 
-  if (!myDFPlayer.begin(mySoftwareSerial, false)) {  //Use softwareSerial to communicate with mp3.
+  wifiManager.autoConnect("Rabbit");
+  Serial.println("Rabbit connected to WiFi :)");
+
+  // this sets the MQTT client and its callback
+  Serial.println("Setting MQTT client...");
+  MQTTclient.setServer(MQTT_SERVER, 1883);
+  MQTTclient.setCallback(MQTTcallback);
+  initial_millis = 0;
+
+  if (!myDFPlayer.begin(mySoftwareSerial, true, false)) {  //Use softwareSerial to communicate with mp3.
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
     Serial.println(F("2.Please insert the SD card!"));
     while(true){
-      delay(0); // Code to compatible with ESP8266 watch dog.
+      delay(0); // Code to make it compatible with ESP8266 watch dog.
     }
   }
   Serial.println(F("DFPlayer Mini online."));
 
   myDFPlayer.volume(DFPLAYER_VOLUME);
-  myDFPlayer.play(1);  //Play the first mp3
 
   Serial.println("Ready!");
-
 }
+
+//* LOOP *//
+//* LOOP *//
+//* LOOP *//
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!MQTTclient.connected()) {
 
-  headButton.poll();
-  leftMotorOpto.poll();
-  rightMotorOpto.poll();
-  if(headButton.longPress()) Serial.println("headButton longPress");
-  if(headButton.doubleClick()) Serial.println("headButton doubleClick a");
-  if(headButton.singleClick()) Serial.println("headButton singleClick");
-  if(leftMotorOpto.longPress()) Serial.println("leftMotorOpto longPress");
-  if(leftMotorOpto.doubleClick()) Serial.println("leftMotorOpto doubleClick b");
-  if(leftMotorOpto.singleClick()) Serial.println("leftMotorOpto singleClick");
-  if(rightMotorOpto.longPress()) Serial.println("rightMotorOpto longPress c");
-  if(rightMotorOpto.doubleClick()) Serial.println("rightMotorOpto doubleClick c");
-  if(rightMotorOpto.singleClick()) Serial.println("rightMotorOpto singleClick c");
+    current_millis = millis();
+    if (current_millis - initial_millis > MQTT_DELAY) {
 
-  static unsigned long timer = millis();
+      Serial.println("MQTT not connected...");
+      Serial.println("reconnecting...");
+      initial_millis = current_millis;
 
-  if (millis() - timer > 500) {
-    timer = millis();
-    myDFPlayer.next();  //Play next mp3 every 3 second.
-  }
-
-  if (myDFPlayer.available()) {
-    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
-  }
-
-  // Serial.println("Forward:");
-  // // Put the motor in forward:
-    driver.motorAForward();
-    driver.motorBForward();
-
-    // Serial.println("Colors R, G, B, W...");
-
-    // int adcvalue = 0;
-    // adcvalue = analogRead(PIN_ANALOG_INPUT);
-    // Serial.print("VALUE: "+String(adcvalue));
-
-    // // set the colors,
-    // // if they don't match in order, you need to use NeoGrbFeature feature
-    // strip.SetPixelColor(0, red);
-    // strip.SetPixelColor(1, green);
-    // strip.SetPixelColor(2, blue);
-    // strip.SetPixelColor(3, white);
-    // strip.SetPixelColor(4, red);
-    // strip.SetPixelColor(5, green);
-    // strip.SetPixelColor(6, blue);
-    // strip.SetPixelColor(7, white);
-    // strip.SetPixelColor(8, red);
-    // strip.SetPixelColor(9, green);
-    // strip.SetPixelColor(10, blue);
-    // strip.SetPixelColor(11, white);
-    // strip.SetPixelColor(12, green);
-    // strip.SetPixelColor(13, blue);
-    // strip.SetPixelColor(14, white);
-    // // the following line demonstrates rgbw color support
-    // // if the NeoPixels are rgbw types the following line will compile
-    // // if the NeoPixels are anything else, the following line will give an error
-    // //strip.SetPixelColor(3, RgbwColor(COLOR_SATURATION));
-    // strip.Show();
-
-
-  // Wait to see the effect:
-  // delay(1000);
-
-  // // Pause the motor for stability:
-  // driver.motorAStop();
-  // driver.motorBStop();
-
-  // Serial.println("Reverse:");
-  // // Put the motor in reverse:
-  // driver.motorAReverse();
-  // driver.motorBReverse();
-
-
-  //   Serial.println("Off ...");
-
-  //   // turn off the pixels
-  //   strip.SetPixelColor(0, black);
-  //   strip.SetPixelColor(1, black);
-  //   strip.SetPixelColor(2, black);
-  //   strip.SetPixelColor(3, black);
-  //   strip.SetPixelColor(4, black);
-  //   strip.SetPixelColor(5, black);
-  //   strip.SetPixelColor(6, black);
-  //   strip.SetPixelColor(7, black);
-  //   strip.SetPixelColor(8, black);
-  //   strip.SetPixelColor(9, black);
-  //   strip.SetPixelColor(10, black);
-  //   strip.SetPixelColor(11, black);
-  //   strip.SetPixelColor(12, black);
-  //   strip.SetPixelColor(13, black);
-  //   strip.SetPixelColor(14, black);
-  //   strip.Show();
-
-
-
-  // // Wait to see the effect:
-  // delay(5000);
-
-
-  //   Serial.println("HSL Colors R, G, B, W...");
-
-  //   // set the colors,
-  //   // if they don't match in order, you may need to use NeoGrbFeature feature
-  //   strip.SetPixelColor(0, hslRed);
-  //   strip.SetPixelColor(1, hslGreen);
-  //   strip.SetPixelColor(2, hslBlue);
-  //   strip.SetPixelColor(3, hslWhite);
-  //   strip.Show();
-
-
-  // Serial.println("Stop:");
-  // // Stop the motor:
-  // driver.motorAStop();
-  // driver.motorBStop();
-  // // Wait to see the effect:
-  // delay(5000);
-
-}
-
-
-void printDetail(uint8_t type, int value){
-  switch (type) {
-    case TimeOut:
-      Serial.println(F("Time Out!"));
-      break;
-    case WrongStack:
-      Serial.println(F("Stack Wrong!"));
-      break;
-    case DFPlayerCardInserted:
-      Serial.println(F("Card Inserted!"));
-      break;
-    case DFPlayerCardRemoved:
-      Serial.println(F("Card Removed!"));
-      break;
-    case DFPlayerCardOnline:
-      Serial.println(F("Card Online!"));
-      break;
-    case DFPlayerUSBInserted:
-      Serial.println("USB Inserted!");
-      break;
-    case DFPlayerUSBRemoved:
-      Serial.println("USB Removed!");
-      break;
-    case DFPlayerPlayFinished:
-      Serial.print(F("Number:"));
-      Serial.print(value);
-      Serial.println(F(" Play Finished!"));
-      break;
-    case DFPlayerError:
-      Serial.print(F("DFPlayerError:"));
-      switch (value) {
-        case Busy:
-          Serial.println(F("Card not found"));
-          break;
-        case Sleeping:
-          Serial.println(F("Sleeping"));
-          break;
-        case SerialWrongStack:
-          Serial.println(F("Get Wrong Stack"));
-          break;
-        case CheckSumNotMatch:
-          Serial.println(F("Check Sum Not Match"));
-          break;
-        case FileIndexOut:
-          Serial.println(F("File Index Out of Bound"));
-          break;
-        case FileMismatch:
-          Serial.println(F("Cannot Find File"));
-          break;
-        case Advertise:
-          Serial.println(F("In Advertise"));
-          break;
-        default:
-          break;
+      // Attempt to reconnect
+      if (reconnect()) {
+        initial_millis = 0;
       }
-      break;
-    default:
-      break;
+    }
+  } else {
+    // Client connected
+    MQTTclient.loop();
   }
-
 }
-
